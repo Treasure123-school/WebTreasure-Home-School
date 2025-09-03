6a0fc166d15bb19bd5f27d3d133a42014912498d
Treasure123-school <treasurehomeschool@gmail.com>
2025-09-03 15:10:31 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Create a client
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
+);
