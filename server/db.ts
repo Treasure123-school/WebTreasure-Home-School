acb184cb1d10890c350e8a09cd060d0bf63bcdb3
Treasure123-school <treasurehomeschool@gmail.com>
2025-09-03 18:23:04 +0100

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -8,9 +8,9 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
+  max: 2,
+  idleTimeoutMillis: 10000,
 });
 
 export const db = drizzle(pool, { schema });
